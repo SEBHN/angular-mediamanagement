f7657bd7c33c8d8156b87249cc7f62440c1b65b3
Manuel <wtfjoke@livenet.ch>
2018-11-24 22:32:34 +0100

    reformat spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,41 +1,41 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { FileExplorerComponent} from './file-explorer/file-explorer.component'
-import { CockpitComponent } from './cockpit/cockpit.component'
-import { CreateFolderComponent } from './cockpit/modals/create-folder/create-folder.component'
-import { ElementComponent } from './file-explorer/element/element.component'
-import { ContextMenuModule } from 'ngx-contextmenu';
-import { RenameFileComponent } from './cockpit/modals/rename-file/rename-file.component'
-import { HttpClientModule } from "@angular/common/http";
-import { ModalModule } from 'ngx-bootstrap';
+import {TestBed, async} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {FileExplorerComponent} from './file-explorer/file-explorer.component';
+import {CockpitComponent} from './cockpit/cockpit.component';
+import {CreateFolderComponent} from './cockpit/modals/create-folder/create-folder.component';
+import {ElementComponent} from './file-explorer/element/element.component';
+import {ContextMenuModule} from 'ngx-contextmenu';
+import {RenameFileComponent} from './cockpit/modals/rename-file/rename-file.component';
+import {HttpClientModule} from '@angular/common/http';
+import {ModalModule} from 'ngx-bootstrap';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        FontAwesomeModule, 
-        ContextMenuModule, 
-        HttpClientModule, 
-        ModalModule.forRoot(), 
-        ContextMenuModule.forRoot({
-          useBootstrap4: true,
-        })
-      ],
-      declarations: [
-        AppComponent, 
-        CockpitComponent, 
-        FileExplorerComponent, 
-        CreateFolderComponent, 
-        ElementComponent, 
-        RenameFileComponent
-      ]
-    }).compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                FontAwesomeModule,
+                ContextMenuModule,
+                HttpClientModule,
+                ModalModule.forRoot(),
+                ContextMenuModule.forRoot({
+                    useBootstrap4: true,
+                })
+            ],
+            declarations: [
+                AppComponent,
+                CockpitComponent,
+                FileExplorerComponent,
+                CreateFolderComponent,
+                ElementComponent,
+                RenameFileComponent
+            ]
+        }).compileComponents();
+    }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    it('should create the app', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    });
 });
