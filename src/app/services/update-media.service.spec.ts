66e10d77e571bd3c90ad757c72ef13615856aa4a
Manuel <wtfjoke@livenet.ch>
2018-11-25 19:20:22 +0100

    Fix Test by importing HTTPClientModule

--- src/app/services/update-media.service.spec.ts
+++ src/app/services/update-media.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UpdateMediaService } from './update-media.service';
+import { HttpClientModule } from "@angular/common/http";
 
 describe('UpdateMediaService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({imports: [
+    HttpClientModule
+  ],}));
 
   it('should be created', () => {
     const service: UpdateMediaService = TestBed.get(UpdateMediaService);
