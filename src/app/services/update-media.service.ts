b96acc518115d3c1c73d7b7ca26a28a5a0f4f498
bkyuchuk <bkyuchuk@stud.hs-heilbronn.de>
2018-11-30 21:02:02 +0100

    Refactored update media service

--- src/app/services/update-media.service.ts
+++ src/app/services/update-media.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpErrorResponse, HttpHeaders, HttpResponse} from "@angular/common/http";
+import {HttpClient, HttpHeaders, HttpResponse} from "@angular/common/http";
 import {Media} from "../shared/media.model";
 import {FilesService} from "./files-service.service";
 import {environment} from '../../environments/environment';
@@ -9,34 +9,19 @@ import {environment} from '../../environments/environment';
 })
 export class UpdateMediaService {
 
-    putMediaUrl = "/users/{userId}/media/{id}";
-
     constructor(private http: HttpClient, private filesService: FilesService) { }
 
-    // REST call to post the media Data (meta data)
+    // Media Metadata POST
     putMedia(media: Media) {
-        this.http.put(this.putMediaUrlBuilder(media.id), JSON.stringify(media), {
+        this.http.put(`/users/${environment.currentUserId}/media/${media.id}`, JSON.stringify(media), {
             reportProgress: true,
             observe: 'response',
-            headers: this.getHeaders()
+            headers: {
+                'Content-Type': 'application/json'
+            }
         })
-            .subscribe(response => this.putMediaResponse(response), error1 => console.log(error1));
-    }
-
-    private putMediaResponse(response: HttpResponse<any>){
-      console.log(response);
-      this.filesService.rename(response.body["id"], response.body["name"]);
-    }
-    // returns the post File URL after it replaces the {userID} field and {id} field
-    putMediaUrlBuilder(id: string): string {
-        var url = this.putMediaUrl.replace("{userId}", environment.currentUserId);
-        var url = url.replace("{id}", id);
-        return url;
-    }
-
-    getHeaders(): HttpHeaders {
-        let headers = new HttpHeaders();
-        headers = headers.append('Content-Type', 'application/json');
-        return headers;
+            .subscribe((response) => {
+                this.filesService.rename(response.body["id"], response.body["name"]);
+            }, error => console.log(new Error(error.message)));
     }
 }
