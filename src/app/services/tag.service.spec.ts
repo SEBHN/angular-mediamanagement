474c0b628b07bc5d0583d467eab7dd232060e1f4
Emin Korkmaz <korkmaz.emin@hotmail.de>
2018-12-09 17:50:39 +0100

    fix unit test

--- src/app/services/tag.service.spec.ts
+++ src/app/services/tag.service.spec.ts
@@ -1,12 +1,17 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { TagService } from './tag.service';
+import {TagService} from './tag.service';
+import {HttpClientModule} from "@angular/common/http";
 
 describe('TagService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+    beforeEach(() => TestBed.configureTestingModule({
+        imports: [
+            HttpClientModule
+        ],
+    }));
 
-  it('should be created', () => {
-    const service: TagService = TestBed.get(TagService);
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        const service: TagService = TestBed.get(TagService);
+        expect(service).toBeTruthy();
+    });
 });
