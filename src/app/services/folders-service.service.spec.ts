a96342c0c51893f333cdce20b4f0ee204936b1f6
Johanna Mergler <jmergler@stud.hs-heilbronn.de>
2018-12-22 00:22:59 +0100

    use HttpClientModule

--- src/app/services/folders-service.service.spec.ts
+++ src/app/services/folders-service.service.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FoldersServiceService } from './folders-service.service';
-import {HttpClient, HttpResponse} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('FolersServiceService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({imports: [
+    HttpClientModule
+  ]}));
 
   it('should be created', () => {
     const service: FoldersServiceService = TestBed.get(FoldersServiceService);
