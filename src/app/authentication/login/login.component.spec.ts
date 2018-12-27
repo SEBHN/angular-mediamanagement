057d7aa19de7131f16153e3d43a0bba1dd3db21b
Manuel <wtfjoke@livenet.ch>
2018-12-28 00:17:11 +0100

    Fix tests

--- src/app/authentication/login/login.component.spec.ts
+++ src/app/authentication/login/login.component.spec.ts
@@ -3,6 +3,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
+import { OAuthModule } from 'angular-oauth2-oidc';
+import { FormsModule } from '@angular/forms';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -11,7 +13,16 @@ describe('LoginComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
-      imports: [RouterTestingModule, HttpClientModule]
+      imports: [RouterTestingModule,
+                HttpClientModule,
+                OAuthModule.forRoot({
+                  resourceServer: {
+                    allowedUrls: ['http://localhost:8080', 'https://mvs-18-ws-spring-in-cloud.appspot.com'],
+                    sendAccessToken: true
+                  }
+                }),
+                FormsModule
+              ]
     })
     .compileComponents();
   }));
